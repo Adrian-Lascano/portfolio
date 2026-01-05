15d2c568365e5382c1e8730da00db124dddd0e1b
Adrian-Lascano <92502317+Adrian-Lascano@users.noreply.github.com>
2026-01-05 10:16:03 +0800

    Add files via upload

---
+++ portfolioLinks.js
@@ -0,0 +1,9 @@
+const portfolioLinks = {
+    "https://adrian-lascano.github.io/MAas-Compositions/": {
+        projectName: "MAas Compositions"
+    },
+
+    "https://adrian-lascano.github.io/My-LifeTime/": {
+        projectName: "My LifeTime"
+    }
+};
