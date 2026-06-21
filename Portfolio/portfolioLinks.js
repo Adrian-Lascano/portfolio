a7856ffd362c94c4717baa6b7ed54ddc7ecee69b
Adrian-Lascano <92502317+Adrian-Lascano@users.noreply.github.com>
2026-06-21 12:18:21 +0800

    Add files via upload

---
+++ Portfolio/portfolioLinks.js
@@ -0,0 +1,11 @@
+const portfolioLinks = {
+    "https://adrian-lascano.github.io/MAas-Compositions/": {
+        projectName: "MAas Compositions"
+    },
+    "https://adrian-lascano.github.io/My-LifeTime/": {
+        projectName: "My LifeTime"
+    },
+    "https://github.com/Adrian-Lascano/Bird-Merchant": {
+        projectName: "Bird Merchant"
+    }
+};
